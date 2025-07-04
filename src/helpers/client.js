82040d21ac1afba382cb577e98fe12b721f2ce89
Adonit Halili <adonit.ha12@gmail.com>
2025-07-05 00:57:18 +0200

    fixed routes

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,7 +1,5 @@
-// src/helpers/client.js
-
 import axios from 'axios';
-import { useAuthStore } from '@/stores/authStore'; // Import your auth store
+import { useAuthStore } from '@/stores/authStore';
 
 const client = axios.create({
     baseURL: 'https://online-voting-system-rest-api-5.onrender.com/api/v1/',
@@ -10,35 +8,15 @@ const client = axios.create({
     },
 });
 
-// Request Interceptor: Add the JWT token to every outgoing request
-client.interceptors.request.use(
-    (config) => {
-        const authStore = useAuthStore(); // Get a fresh instance of the store
-        const token = authStore.token; // Access the token from the store
-
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+client.interceptors.request.use((config) => {
+    // Merr store-në në mënyrë manuale, jashtë reactive context
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
     }
-);
-
-client.interceptors.request.use(
-    (config) => {
-        const authStore = useAuthStore(); // ❌ Problem këtu
-        const token = authStore.token;
-
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default client;
