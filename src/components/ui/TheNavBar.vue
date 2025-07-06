9995fd4ad3fdcba9d56feb0c1120dff332643a33
Adonit Halili <adonit.ha12@gmail.com>
2025-07-06 21:35:44 +0200

    changed navbar

--- src/components/ui/TheNavBar.vue
+++ src/components/ui/TheNavBar.vue
@@ -1,86 +1,109 @@
+// TheNavBar.vue
 <script setup>
-import { ref } from "vue";
 
-const emit = defineEmits(["toggle-sidebar"]);
+import {useAuthStore} from "@/stores/authStore.js";
+import {useRouter} from "vue-router";
+import {useAppToast} from "@/composables/useAppToast.js";
+import {toFormData} from "axios";
 
-const showProfileDropdown = ref(false);
-const toggleProfileDropdown = () => {
-  showProfileDropdown.value = !showProfileDropdown.value;
-};
+const authStore = useAuthStore();
+const router = useRouter()
+const toast = useAppToast()
 
-const showSidebarDropdown = ref(false);
-const toggleSidebarDropdown = () => {
-  showSidebarDropdown.value = !showSidebarDropdown.value;
-};
+const onLogout = () => {
+  authStore.logout()
+  router.push({name: 'login'})
+  toast.showSuccess("You're logged out!");
+}
+
+
 </script>
 
 <template>
   <div class="main-header">
-    <!-- Mobile Header -->
-    <div
-        class="logo-header d-block d-lg-none px-3 py-2 d-flex align-items-center justify-content-between"
-        data-background-color="dark"
+    <!-- Navbar Header -->
+    <nav
+        class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom"
+        data-background-color="white"
     >
-      <!-- Logo -->
-      <router-link :to="{ name: 'home' }" class="logo d-flex align-items-center">
-        <img
-            src="@/assets/img/foto/ks.jpeg"
-            alt="navbar brand"
-            class="navbar-brand"
-            height="40"
-        />
-      </router-link>
-
-      <!-- Sidebar Toggle Button me event dhe opsional dropdown -->
-      <button class="btn btn-sm text-white" @click="emit('toggle-sidebar')">
-        <i class="gg-menu-left fs-5"></i>
-      </button>
-
-      <!-- Shembull: Sidebar Toggle Dropdown (opsionale, nëse e do) -->
-      <!--
-      <button class="btn btn-sm text-white" @click="toggleSidebarDropdown">
-        <i class="gg-menu-left fs-5"></i>
-      </button>
-      <ul v-if="showSidebarDropdown" class="dropdown-menu">
-        <li>Option 1</li>
-        <li>Option 2</li>
-      </ul>
-      -->
-
-      <!-- Profile Button -->
-      <div class="position-relative">
-        <button class="btn btn-sm text-white" @click="toggleProfileDropdown">
-          <i class="gg-more-vertical-alt fs-5"></i>
-        </button>
-
-        <ul
-            v-if="showProfileDropdown"
-            class="dropdown-menu dropdown-user show animated fadeIn"
-            style="right: 0; left: auto"
+      <div class="container-fluid">
+        <nav
+            class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex"
         >
-          <li>
-            <div class="user-box text-center p-2">
-              <h6>{{ /* Emri i user-it */ }}</h6>
-              <p class="text-muted small">{{ /* Email apo user sub */ }}</p>
-              <router-link
-                  :to="{ name: 'my-profile' }"
-                  class="btn btn-sm btn-secondary mb-1"
-              >View Profile</router-link
-              >
-            </div>
-          </li>
-          <li>
-            <div class="dropdown-divider"></div>
-            <router-link
-                :to="{ name: 'change-password' }"
-                class="dropdown-item"
-            >Change Password</router-link
+        </nav>
+
+        <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
+          <li
+              class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none"
+          >
+            <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-expanded="false"
+                aria-haspopup="true"
             >
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" @click="/* Logout funksioni */">Logout</a>
+              <i class="fa fa-search"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-search animated fadeIn">
+              <form class="navbar-left navbar-form nav-search">
+                <div class="input-group">
+                  <input
+                      type="text"
+                      placeholder="Search ..."
+                      class="form-control"
+                  />
+                </div>
+              </form>
+            </ul>
+          </li>
+          <li class="nav-item topbar-user dropdown hidden-caret">
+            <a
+                class="dropdown-toggle profile-pic"
+                data-bs-toggle="dropdown"
+                href="#"
+                aria-expanded="false"
+            >
+
+              <span class="profile-username">
+                      <span class="op-7">Hi, </span>
+                      <span class="fw-bold">{{
+                          authStore.loggedInUser?.firstName + " " + authStore.loggedInUser?.lastName
+                        }}</span>
+                    </span>
+            </a>
+            <ul class="dropdown-menu dropdown-user animated fadeIn">
+              <div class="dropdown-user-scroll scrollbar-outer">
+                <li>
+                  <div class="user-box">
+
+                    <div class="u-text">
+                      <h4>{{ authStore.loggedInUser?.firstName + " " + authStore.loggedInUser?.lastName }}</h4>
+                      <p class="text-muted">{{ authStore.loggedInUser?.sub }}</p>
+                      <router-link
+                          :to="{name:'my-profile'}"
+                          class="btn btn-xs btn-secondary btn-sm"
+                      >View Profile
+                      </router-link
+                      >
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="dropdown-divider"></div>
+
+                  <router-link :to="{name:'change-password'}" class="dropdown-item" href="#">Change Password</router-link>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" @click="onLogout">Logout</a>
+                </li>
+              </div>
+            </ul>
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
+    <!-- End Navbar -->
   </div>
+
 </template>
